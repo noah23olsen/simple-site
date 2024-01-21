dd0fb5715a32b287204a97b06f4d32474ffdbb9d
noah23olsen <noah23olsen@gmail.com>
2024-01-21 11:42:50 -0500

    add vercel analytics, add new favicon/title

--- simple-site-vue-project/src/main.js
+++ simple-site-vue-project/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/router';
+import { inject } from '@vercel/analytics';
 
+inject();
 createApp(App)
   .use(router)
   .mount('#app');
