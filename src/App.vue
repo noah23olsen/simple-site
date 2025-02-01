57097bc3d94602a6fb450190f2392bb60766faab
Noah Olsen <noah23olsen@gmail.com>
2025-02-01 15:27:45 -0500

    fix navbar and some styling

--- src/App.vue
+++ src/App.vue
@@ -5,37 +5,38 @@
         <title-vue :titleText="currentTitle" />
       </div>
     </div>
-    <div class="row d-flex bg-cyan">
-      <router-link to="/">home</router-link>
-      <router-link to="/music">listens</router-link>
-      <router-link to="/readings">readings</router-link>
+    <div class="row d-flex flex-row bg-cyan justify-content-center text-center py-3">
+      <div class="col-4"> <router-link to="/">home</router-link></div>
+      <div class="col-4"> <router-link to="/music">listens</router-link></div>
+      <div class="col-4"> <router-link to="/readings">readings</router-link></div>
     </div>
-    <div class="row bg-cyan p-5">
+    <div class="row bg-cyan px-5">
       <div class="col-12 bg-white p-5">
         <router-view></router-view>
       </div>
     </div>
-    <div class="row text-center bg-cyan">
-      <div class="col-12"></div>
-      <p>inspired by <a href="https://ranprieur.com/">ran prieur</a></p>
+    <div class="row text-center bg-cyan d-flex justify-content-center align-items-center">
+      <div class="col-12 py-3">
+        <p>inspired by <a href="https://ranprieur.com/">ran prieur</a></p>
+      </div>
     </div>
   </div>
 
   <div v-else>
     <div class="bg-black d-flex justify-content-center align-items-center" style="height: 93vh;">
       <a class="text-white" href="https://www.google.com/search?q=the+answer+to+life%2C+the+universe%2C+and+everything"
-      target="_blank">42
-    </a>
-  </div>
-  
-  <div class="bg-black d-flex align-items-center justify-content-center" style="height: 7vh; position: relative;">
-    <div class="d-flex w-50 justify-content-between">
-      <a class="links-color" href="https://github.com/noah23olsen" target="_blank">github</a>
-      <a class="links-color" href="https://twitter.com/noaholsen_" target="_blank">x</a>
-      <a class="links-color" href="https://www.linkedin.com/in/noaholsen-/" target="_blank">linkedin</a>
+        target="_blank">42
+      </a>
+    </div>
+
+    <div class="bg-black d-flex align-items-center justify-content-center" style="height: 7vh;]">
+      <div class="d-flex w-50 justify-content-between">
+        <a class="links-color" href="https://github.com/noah23olsen" target="_blank">github</a>
+        <a class="links-color" href="https://twitter.com/noaholsen_" target="_blank">x</a>
+        <a class="links-color" href="https://www.linkedin.com/in/noaholsen-/" target="_blank">linkedin</a>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -9,3 +9,4 @@ inject();
 createApp(App)
   .use(router)
   .mount('#app');
+
